4f53bc262233ab1f602f449efe9a86a2deb19d60
KazuyaTakyu <kaztak_yh@yahoo.co.jp>
2024-12-30 00:51:54 +0900

    v0.2.1.3 実機確認用モジュール追加、gradleの依存関係更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.21" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.library") version "8.6.0" apply false
 //    id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
     id("maven-publish")
     kotlin("jvm") version "1.9.21" apply false
